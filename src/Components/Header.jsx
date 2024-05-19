a20cdd5478a7ef16b1b1be25b4c530a4224b823d
himattheww <matthewionwyn07@gmail.com>
2024-05-20 01:11:18 +0700

    Add assets

--- src/Components/Header.jsx
+++ src/Components/Header.jsx
@@ -16,12 +16,11 @@ import {
   AccountCircle,
   Menu as MenuIcon,
 } from "@mui/icons-material";
-import Swal from "sweetalert2"; // Impor SweetAlert2
-import logoWibushop from "../Assets/Logo/logo2.webp"; // Ganti dengan path gambar yang sesuai
+import Swal from "sweetalert2";
+import logoWibushop from "../Assets/Logo/logo2.webp";
 
 const Header = ({ handleToggleSize }) => {
   const [anchorEl, setAnchorEl] = React.useState(null);
-  const [shoppingAnchorEl, setShoppingAnchorEl] = React.useState(null);
 
   const handleMenuOpen = (event) => {
     setAnchorEl(event.currentTarget);
@@ -32,31 +31,23 @@ const Header = ({ handleToggleSize }) => {
   };
 
   const handleMenuClick = () => {
-    handleToggleSize(); // Memanggil fungsi toggle size dari props
-  };
-
-  const handleShoppingMenuOpen = (event) => {
-    setShoppingAnchorEl(event.currentTarget);
-  };
-
-  const handleShoppingMenuClose = () => {
-    setShoppingAnchorEl(null);
+    handleToggleSize();
   };
 
   const handleDownloadClick = () => {
     Swal.fire({
-      title: 'Still on development',
-      text: 'Please wait for the next update',
-      icon: 'info',
-      confirmButtonText: 'OK',
-      background: '#333',
-      color: '#fff',
-      confirmButtonColor: '#ff4081',
+      title: "Still on development",
+      text: "Please wait for the next update",
+      icon: "info",
+      confirmButtonText: "OK",
+      background: "#333",
+      color: "#fff",
+      confirmButtonColor: "#ff4081",
       customClass: {
-        popup: 'swal-popup',
-        title: 'swal-title',
-        content: 'swal-content',
-        confirmButton: 'swal-confirm-button',
+        popup: "swal-popup",
+        title: "swal-title",
+        content: "swal-content",
+        confirmButton: "swal-confirm-button",
       },
     });
   };
@@ -68,9 +59,6 @@ const Header = ({ handleToggleSize }) => {
     >
       <Toolbar sx={{ justifyContent: "space-between" }}>
         <div style={{ display: "flex", alignItems: "center" }}>
-          <IconButton sx={{ color: "#fff" }} onClick={handleMenuClick}>
-            <MenuIcon />
-          </IconButton>
           <Link href="/home" underline="none">
             <img
               src={logoWibushop}
@@ -95,64 +83,11 @@ const Header = ({ handleToggleSize }) => {
                 Daftar Anime
               </Typography>
             </Link>
-            <Typography
-              variant="h6"
-              sx={{ color: "#ff4081", ml: 2, cursor: "pointer" }}
-              onClick={handleShoppingMenuOpen}
-            >
-              Shopping
-            </Typography>
-            <Menu
-              id="shopping-menu"
-              anchorEl={shoppingAnchorEl}
-              open={Boolean(shoppingAnchorEl)}
-              onClose={handleShoppingMenuClose}
-              PaperProps={{
-                style: {
-                  backgroundColor: "#444",
-                  color: "#fff",
-                  borderRadius: "8px",
-                  minWidth: "150px",
-                },
-              }}
-            >
-              <MenuItem
-                onClick={handleShoppingMenuClose}
-                component={Link}
-                href="/shop/manga"
-                sx={{
-                  "&:hover": {
-                    backgroundColor: "#555",
-                  },
-                }}
-              >
-                Manga
-              </MenuItem>
-              <MenuItem
-                onClick={handleShoppingMenuClose}
-                component={Link}
-                href="/shop/accessories"
-                sx={{
-                  "&:hover": {
-                    backgroundColor: "#555",
-                  },
-                }}
-              >
-                Accessories
-              </MenuItem>
-              <MenuItem
-                onClick={handleShoppingMenuClose}
-                component={Link}
-                href="/shop/fashion"
-                sx={{
-                  "&:hover": {
-                    backgroundColor: "#555",
-                  },
-                }}
-              >
-                Fashion
-              </MenuItem>
-            </Menu>
+            <Link href="/shop" underline="none">
+              <Typography variant="h6" sx={{ color: "#ff4081", ml: 2 }}>
+                Shopping
+              </Typography>
+            </Link>
           </Box>
         </div>
         <div style={{ display: "flex", alignItems: "center" }}>
@@ -194,7 +129,7 @@ const Header = ({ handleToggleSize }) => {
             }}
           >
             <MenuItem
-            // buat comoponent = {Link untuk menandakan dia Link}
+              // buat comoponent = {Link untuk menandakan dia Link}
               component={Link}
               href="/profile"
               onClick={handleMenuClose}
